97523bfe066d7c4d875f6ac79dcab2323d5368ca
andre-gadelha <adrrda@gmail.com>
2025-10-27 14:49:26 -0300

    Aicionando códi

---
+++ src/main/java/br/com/voluntechs/api_electro_route/ApiElectroRouteApplication.java
@@ -0,0 +1,13 @@
+package br.com.voluntechs.api_electro_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiElectroRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiElectroRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/voluntechs/api_electro_route/ApiElectroRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voluntechs.api_electro_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiElectroRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
